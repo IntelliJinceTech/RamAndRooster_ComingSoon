a361e3837c162ada8b25d31501b633a0007bec64
Christopher Jin <chrisjin1@gmail.com>
2024-04-18 15:33:57 -0400

    added xs breakpoint on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
   content: ['./index.html', './*.{html,js}'],
   theme: {
+    screens: {
+      xs: '310px',
+      ...defaultTheme.screens,
+    },
     extend: {
       fontFamily: {
         quint: ['Quintessential', 'serif'],
